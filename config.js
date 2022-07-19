68c53449d3f3ff0de19fc43ce317521bc5b0e4ee
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-07-19 20:33:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,10 +18,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
+global.owner = ['6281322577123','6281322577123','6281322577123','6281322577123']
+global.premium = ['6281322577123']
+global.packname = 'Sad Botz'
+global.author = 'Lintang'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
